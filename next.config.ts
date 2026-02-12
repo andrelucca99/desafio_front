908d86d69b8b407c457862bafff97720fd32850b
André Lucas <andrelucca99@gmail.com>
2026-02-12 05:32:48 -0300

    feat: configura domínios permitidos para imagens externas no next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "escoramento.com",
+      },
+      {
+        protocol: "https",
+        hostname: "escoramento-files.s3.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
