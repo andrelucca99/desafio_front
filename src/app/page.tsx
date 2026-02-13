7b3fe8e332b9e254e64f2ee263dcbf52757db574
André Lucas <andrelucca99@gmail.com>
2026-02-13 09:28:53 -0300

    style: ajustes no responsivo dos compenentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col sm:flex-row gap-2 justify-center items-center my-15">
-          <input className="border border-gray-500 w-[580px] rounded-[2px] px-7 py-4" type="text" placeholder="O que você está procurando?" />
+          <input className="border border-gray-500 w-full sm:w-[580px] rounded-[2px] px-7 py-4" type="text" placeholder="O que você está procurando?" />
 
           <button className="w-full h-full sm:w-36 sm:auto p-2 bg-[#006E3A] text-white rounded-[2px]">
             BUSCAR
@@ -83,10 +83,10 @@ export default function Home() {
         <section className="w-full bg-[#F4F6F8] py-2 px-5">
           <TitleSection>Recursos</TitleSection>
           <div className="flex w-full flex-col sm:flex-row bg-transparent gap-3 border border-gray-400 rounded-2xl">
-            <div className="w-3/4 flex flex-col justify-between py-8 px-4">
+            <div className="w-full md:w-3/4 flex flex-col justify-between gap-4 py-8 px-4">
               <span className="text-sm font-bold text-gray-500">Blog e Vídeos</span>
 
-              <h3 className="text-3xl font-bold">
+              <h3 className="text-[20px] md:text-3xl font-bold">
                 Acesse conteúdo técnico também através do nosso blog e em nosso canal de vídeos
               </h3>
 
@@ -96,7 +96,7 @@ export default function Home() {
 
               <Button text="ACESSAR CONTEÚDO" />
             </div>
-            <Image className="rounded-r-2xl" src={foto} alt="Recursos" width={545} height={310} />
+            <Image className="rounded-b-2xl md:rounded-r-2xl" src={foto} alt="Recursos" width={545} height={310} />
           </div>
         </section>
 
--- src/components/ServiceModel/index.tsx
+++ src/components/ServiceModel/index.tsx
@@ -14,27 +14,36 @@ export default function ServiceModel({ image, title, description, textButton, cl
   const isReversed = className?.includes("reverse");
 
   return (
-    <section className={`flex w-full relative ${className || ""} z-10`}>
+    <section
+      className={`w-full flex flex-col md:flex-row relative ${className || ""} z-10`}
+    >
       {isReversed ? (
         <>
-          <div className="w-1/2 bg-gray-300"></div>
-          <div className="w-full">
+          <div className="w-full md:w-1/2 bg-gray-300 order-3 md:order-1"></div>
+
+          <div className="w-full order-1 md:order-2">
             <Image src={image} alt={title} />
           </div>
         </>
       ) : (
         <>
-          <div className="w-full">
+          <div className="w-full order-1 md:order-1">
             <Image src={image} alt={title} />
           </div>
-          <div className="w-1/2 bg-gray-300"></div>
+
+          <div className="w-full md:w-1/2 bg-gray-300 order-3 md:order-2"></div>
         </>
       )}
+
       <div
-        className={`absolute ${isReversed ? "top-1/2 left-1/4" : "top-1/2 left-3/4"
-          } transform -translate-x-1/2 -translate-y-1/2 w-[450px] max-h-fit bg-white rounded-2xl py-6 px-8 flex flex-col justify-around gap-6 shadow-lg`}
+        className={`absolute ${isReversed
+          ? "md:top-1/2 md:left-1/4"
+          : "md:top-1/2 md:left-3/4"
+          } top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full md:w-[450px] max-h-fit bg-white rounded-2xl py-6 px-8 flex flex-col justify-around gap-6 shadow-lg order-2`}
       >
-        <h3 className="text-[28px] font-bold leading-normal text-[#212121]">{title}</h3>
+        <h3 className="text-[20px] sm:text-[28px] font-bold leading-normal text-[#212121]">
+          {title}
+        </h3>
         <Paragraph>{description}</Paragraph>
         <Button text={textButton} />
       </div>
