7c6e3c44d19c1e9ba012429ada4e5f6bbf8dd244
André Lucas <andrelucca99@gmail.com>
2026-02-14 16:46:43 -0300

    feat: implementa componente Icon

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,17 @@
+import { ElementType } from "react";
+
+interface IconProps {
+  Icon: ElementType;
+  bgHover?: string;
+}
+
+export default function Icon({
+  Icon,
+  bgHover = "hover:bg-[#134f2e5c]",
+}: IconProps) {
+  return (
+    <div className={`flex items-center justify-center w-10 h-10 rounded-full bg-transparent ${bgHover} transition-colors duration-300 cursor-pointer`}>
+      <Icon className="w-5 h-5 text-white" />
+    </div>
+  );
+}
