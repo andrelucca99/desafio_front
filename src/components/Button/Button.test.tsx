b816ca93080647dc29732c40308f2b7e9e1134ca
André Lucas <andrelucca99@gmail.com>
2026-02-15 08:47:41 -0300

    test: add teste para o componente Button

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./index";
+
+describe('Teste componente Button', () => {
+  it('Verifica se o componente é renderizado e recebe o texto via props', () => {
+    render(<Button text="Acessar Conteúdo" />);
+
+    const btnText = screen.getByRole("button", { name: /Acessar Conteúdo/i });
+
+    expect(btnText).toBeInTheDocument();
+  })
+})
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,9 @@ interface IButton {
 
 export default function Button({ text }: IButton) {
   return (
-    <button className="uppercase w-[200px] text-[14px] font-bold p-2 bg-[#006E3A] text-white rounded-[4px]">
+    <button
+      data-testid="btn-multi"
+      className="uppercase w-[200px] text-[14px] font-bold p-2 bg-[#006E3A] text-white rounded-[4px] hover:cursor-pointer">
       {text}
     </button>
   );
