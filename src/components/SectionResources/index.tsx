0c2f5d4e7d8eaaff1f919231b0326e520e489538
André Lucas <andrelucca99@gmail.com>
2026-02-16 09:17:12 -0300

    style: ajuste no resposivo do componente SectionResources

--- src/components/SectionResources/index.tsx
+++ src/components/SectionResources/index.tsx
@@ -10,8 +10,8 @@ export default function SectionResources() {
   return (
     <section className="w-full bg-[#F4F6F8] py-2 px-5">
       <TitleSection>Recursos</TitleSection>
-      <div className="flex w-full flex-col sm:flex-row bg-transparent gap-3 border border-gray-400 rounded-2xl">
-        <div className="w-full md:w-3/4 flex flex-col justify-between gap-4 py-8 px-4">
+      <div className="flex w-full flex-col lg:flex-row bg-transparent gap-3 border border-[#c7c7c7] rounded-2xl">
+        <div className="w-full lg:w-3/4 flex flex-col justify-between gap-4 py-8 px-4">
           <span className="text-sm font-bold text-gray-500">Blog e Vídeos</span>
 
           <h3 className="text-[20px] md:text-3xl font-bold">
@@ -24,7 +24,9 @@ export default function SectionResources() {
 
           <Button text="acessar conteúdo" />
         </div>
-        <Image className="rounded-b-2xl md:rounded-r-2xl" src={foto} alt="Recursos" width={545} height={310} />
+        <div className="w-full lg:w-[545px]">
+          <Image className="h-full sm:rounded-bl-2xl sm:rounded-br-2xl lg:rounded-b-none lg:rounded-r-2xl lg:rounded-br-2xl" src={foto} alt="Recursos" />
+        </div>
       </div>
     </section>
   );
