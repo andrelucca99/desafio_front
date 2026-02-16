cda4becf891b85357a37f0bd1a6020c3639b76bd
André Lucas <andrelucca99@gmail.com>
2026-02-16 13:46:28 -0300

    style: ajustes nos estilos do componente Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import logo from "../../assets/logo.png";
 import ListMenuFooter from "../ListMenuFooter";
 import { ShoppingCartIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { TbWorld } from "react-icons/tb";
 
 interface ISidebar {
   open: boolean;
@@ -20,11 +21,11 @@ export default function Sidebar({ open, setOpen }: ISidebar) {
       />
 
       <aside
-        className={`fixed top-0 left-0 h-full w-4/5 bg-white z-50 shadow-xl
-        transform transition-transform duration-300 ease-in-out
+        className={`fixed top-0 left-0 h-full w-4/5 md:w-2/5 bg-white z-50 shadow-xl
+        transform transition-transform duration-300 ease-in-out flex flex-col
         ${open ? "translate-x-0" : "-translate-x-full"}`}
       >
-        <div className="flex flex-col justify-between items-center p-4 border-b">
+        <div className="flex flex-col justify-between items-center p-4">
           <button
             className="first:ml-auto"
             onClick={() => setOpen(false)}
@@ -32,47 +33,53 @@ export default function Sidebar({ open, setOpen }: ISidebar) {
             <XMarkIcon className="w-6 h-6" />
           </button>
 
-          <div className="p-4">
-            <Image src={logo} alt="Escoramento.com" priority />
+          <div className="py-4 px-2">
+            <Image src={logo} alt="Escoramento.com" />
           </div>
 
-          <div className="w-full flex justify-between px-5 items-center">
-            <div>
+          <div className="w-full flex justify-between items-center px-2">
+            <div className="flex gap-1 items-center text-[#838383] text-[18px]">
+              <TbWorld className="w-5 h-5" />
               <p>PT</p>
             </div>
             <div className="flex items-center py-2 px-3 border border-gray-400 rounded-3xl cursor-pointer group hover:border-green-700">
-              <ShoppingCartIcon className="w-5 h-5 text-gray-700 group-hover:text-green-700" />
-              <p className="pl-2 text-xs text-gray-700 group-hover:text-green-700">
+              <ShoppingCartIcon className="w-5 h-5 text-gray-700" />
+              <p className="pl-2 text-xs text-gray-700">
                 LOJA
               </p>
             </div>
           </div>
         </div>
 
-        <nav className="flex flex-col gap-2 p-4 overflow-y-auto h-[calc(100vh-65px)]">
+        <nav className="flex flex-col gap-2 p-4 flex-1 overflow-y-auto">
           <ListMenuFooter
             title="Equipamentos"
             items={["Locação", "Venda"]}
+            className="text-[#838383]"
           />
 
           <ListMenuFooter
             title="Material Técnico"
             items={["Catálogos", "Blog"]}
+            className="text-[#838383]"
           />
 
           <ListMenuFooter
             title="Sobre"
             items={["Empresa", "Política de Privacidade", "Termos Gerais de Uso"]}
+            className="text-[#838383]"
           />
 
           <ListMenuFooter
             title="App Cliente"
             items={["Orçamentos", "Agendamento Online", "Cadastro"]}
+            className="text-[#838383]"
           />
 
           <ListMenuFooter
             title="Canais de Atendimento"
             items={["Chat com Especialista"]}
+            className="text-[#838383]"
           />
         </nav>
       </aside>
